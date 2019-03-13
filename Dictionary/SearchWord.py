e979524ab4e53f7ac1a28b6a3986cfbe01f55b4e
JoshHodge <joshhodgkinson90@gmail.com>
2019-03-13 12:52:51 +0200

    Created get_close_matches logic to find similar words

--- Dictionary/SearchWord.py
+++ Dictionary/SearchWord.py
@@ -1,16 +1,19 @@
 import json
 import difflib
 from difflib import get_close_matches
+from difflib import SequenceMatcher
 """
 This function is used to search for a particular word and return the result from the dictionary data set
 """
 data = json.load(open("076 data.json"))
-dkeys = data.keys() #get data kays from dictionary
+dkeys = data.keys() #get data keys from dictionary
 
 def Def_word(word):
     if word in dkeys:
         return data[word.lower()]
+    elif get_close_matches(word, dkeys)[0] != "":
+        return ("Did you mean: " + get_close_matches(word, dkeys)[0] + '?')
     else: return "Word does not exist"
-
+    
 WordToDef = (input("Please enter a word to search for: \n"))
 print(Def_word(WordToDef))
