44507a9de06fea23bd5affe6ac9a0b45589b198c
JoshHodge <joshhodgkinson90@gmail.com>
2019-03-13 15:05:34 +0200

    Added list and string output conditions

--- Dictionary/SearchWord.py
+++ Dictionary/SearchWord.py
@@ -11,10 +11,15 @@ dkeys = data.keys() #get data keys from dictionary
 def Def_word(word):
     if word in dkeys:
         return data[word.lower()]
-    elif get_close_matches(word, dkeys)[0] != "":
+    elif get_close_matches(word, dkeys, cutoff=0.6)[0] != "":
         NewWord = get_close_matches(word, dkeys)[0]
         return ("Did you mean: " + NewWord + "? \n" + "The definition for " + NewWord + " is: " + str(data[NewWord.lower()]).strip('[]'))
     else: return "Word does not exist"
 
 WordToDef = (input("Please enter a word to search for: \n"))
-print(Def_word(WordToDef))
+output = Def_word(WordToDef)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else: print(output)
