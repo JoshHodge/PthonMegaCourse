f55435cef58d44c8ba707b081c6d54e176a6d19c
JoshHodge <joshhodgkinson90@gmail.com>
2019-03-13 12:00:46 +0200

    Updated to include Data Key search for optimized searching

--- Dictionary/SearchWord.py
+++ Dictionary/SearchWord.py
@@ -1,12 +1,14 @@
 import json
-
+import difflib
+from difflib import get_close_matches
 """
 This function is used to search for a particular word and return the result from the dictionary data set
 """
 data = json.load(open("076 data.json"))
+dkeys = data.keys() #get data kays from dictionary
 
 def Def_word(word):
-    if word in data:
+    if word in dkeys:
         return data[word.lower()]
     else: return "Word does not exist"
 
