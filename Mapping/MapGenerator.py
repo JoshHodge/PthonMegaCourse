a90ed5df133f7ad088d4b7b4e8b220ae3a8c9925
JoshHodge <joshhodgkinson90@gmail.com>
2019-03-14 19:28:18 +0200

    Map1 updated with test marker

--- Mapping/MapGenerator.py
+++ Mapping/MapGenerator.py
@@ -1,3 +1,5 @@
 import folium
 map = folium.Map(location=[-30.5,22.9], zoom_start=6)
+map.add_child(folium.Marker(location=[-30.5,22.9], popup="Starting Marker", icon=folium.Icon(color='green')))
+
 map.save("Map1.html")
