203172103c262edc768bdbca4a8d2dbceff37ef4
JoshHodge <joshhodgkinson90@gmail.com>
2019-03-14 19:48:50 +0200

    Created feature group option for map layers and markers

--- Mapping/MapGenerator.py
+++ Mapping/MapGenerator.py
@@ -1,5 +1,10 @@
 import folium
 map = folium.Map(location=[-30.5,22.9], zoom_start=6)
-map.add_child(folium.Marker(location=[-30.5,22.9], popup="Starting Marker", icon=folium.Icon(color='green')))
+# map.add_child(folium.Marker(location=[-30.5,22.9],popup="This is the starting point", icon=folium.Icon(color='blue')))
 
-map.save("Map1.html")
+#For markers it is easier to create a feature gorup variable to control the look of the icon markers
+fg= folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[-30.5,22.9],popup="This is the starting point", icon=folium.Icon(color='blue')))
+map.add_child(fg)
+
+map.save("MapMarked.html")
