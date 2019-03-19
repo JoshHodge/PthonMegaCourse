d2f0f10fff6e26a79e269d45c389c1e96fd3e816
JoshHodge <joshhodgkinson90@gmail.com>
2019-03-19 20:34:03 +0200

    Changed map to CartoDB Dark_matter Added Railway overlay

--- Mapping/MapGenerator.py
+++ Mapping/MapGenerator.py
@@ -4,7 +4,12 @@ import pandas
 
 data=pandas.read_csv("ProjList.csv",sep=";",error_bad_lines=False)
 
-map = folium.Map(location=[-30.5,22.9], zoom_start=6)
+map = folium.Map(location=[-25.5,22.9], zoom_start=5,tiles='CartoDB dark_matter')
+
+#mapRail = folium.Map.add_child(location=[-30.5,22.9], zoom_start=6, tiles='http://{s}.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png', attr="<a href='https://www.openstreetmap.org/copyright'>© OpenStreetMap contributors</a>, Style='<a href='http://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA 2.0</a> <a href='http://www.openrailwaymap.org/'>OpenRailwayMap</a> and OpenStreetMap")
+#Style: <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA 2.0</a> <a href="http://www.openrailwaymap.org/">OpenRailwayMap</a> and OpenStreetMap
+#tiles='CartoDB dark_matter'
+#tiles="'http://{s}.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png'", attr="<a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors</a>"
 # map.add_child(folium.Marker(location=[-30.5,22.9],popup="This is the starting point", icon=folium.Icon(color='blue')))
 fg= folium.FeatureGroup(name="My Map")
 
@@ -12,24 +17,29 @@ fg= folium.FeatureGroup(name="My Map")
 datakeys = data.keys()
 for i in datakeys :
     print(i)
-coordinates = data["Location"]
-names = data["Client"]
+
+coordinates = list(data["Location"])
+names = list(data["Client"])
 
 #CData = data["Client"] + ";" + data["location"]
-CData = zip(data["Index"],data["Client"],data["Location"])
+CData = zip(names,coordinates)
 
 
-#need to update the popup function to select the correct naem of the company/mine
+#need to update the popup function to select the correct name of the company/mine
 def CreateMarkers(coordinates):
-    for i in coordinates:
-        if len(str(i)) > 3:
-            POI=geopy.Point(i)
-            fg.add_child(folium.Marker(location=[POI.latitude,POI.longitude],popup="This is the starting point", icon=folium.Icon(color='blue')))
+    for nm,co in CData:
+        if len(str(co)) > 3:
+            print(co)
+            POI=geopy.Point(co)
+            fg.add_child(folium.CircleMarker(location=[POI.latitude,POI.longitude],radius=6,popup=nm, fill_color = 'blue',color='blue',fill_opacity=0.6))
             map.add_child(fg)
 
 #For markers it is easier to create a feature gorup variable to control the look of the icon markers
 
 #fg.add_child(folium.Marker(location=[-30.5,22.9],popup="This is the starting point", icon=folium.Icon(color='blue')))
-#map.add_child(fg)
+
+map.add_child(folium.TileLayer(tiles='http://{s}.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png', attr="<a href='https://www.openstreetmap.org/copyright'>© OpenStreetMap contributors</a>, '<a href='http://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA 2.0</a> <a href='http://www.openrailwaymap.org/'>OpenRailwayMap</a> and OpenStreetMap"))
+map.add_child(folium.LayerControl())
 CreateMarkers(coordinates)
+
 map.save("MapMarked.html")
